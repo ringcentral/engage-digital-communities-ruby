b81b3808c78690c3e0ff4db12c964413cfea4744
Jean Boussier <jean.boussier@dimelo.com>
2011-04-13 09:50:40 +0200

    feature: Dimelo::API::BasicObject

---
+++ lib/dimelo/api/basic_object.rb
@@ -0,0 +1,17 @@
+module Dimelo
+  module API
+    
+    class BasicObject
+      # http://ruby-doc.org/core-1.9/classes/BasicObject.html
+      # http://sequel.heroku.com/2010/03/31/sequelbasicobject-and-ruby-18/
+      KEEP_METHODS = %w(__id__ __send__ instance_eval == equal? initialize method_missing respond_to?)
+      
+      def self.remove_methods!
+        m = (private_instance_methods + instance_methods) - KEEP_METHODS
+        m.each{|m| undef_method(m)}
+      end
+      remove_methods!
+    end if not defined?(BasicObject)
+    
+  end
+end
