2247973846d796b9af88b1d4a9877c90b45cf967
Jean Boussier <jean.boussier@dimelo.com>
2011-04-18 19:25:17 +0200

    fix: add Answer#comment_count

--- app/models/dimelo/answer.rb
+++ app/models/dimelo/answer.rb
@@ -3,8 +3,8 @@ module Dimelo
     
     path '/questions/%{question_id}/answers/%{id}'
     
-    attributes :id, :body, :body_format, :flow_state, :user_id, :permalink, :created_at, :updated_at, :question_id
-    submit_attributes :body, :user_id
+    attributes :id, :body, :body_format, :flow_state, :user_id, :permalink, :created_at, :updated_at, :question_id, :comments_count
+    submit_attributes :body, :user_id, :question_id
     
     belongs_to :user
     belongs_to :question
