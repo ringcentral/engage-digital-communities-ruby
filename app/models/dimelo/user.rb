fb931e8b30791e43c1e1078d39f9d015bec6c749
Jean Boussier <jean.boussier@dimelo.com>
2011-04-19 10:11:01 +0200

    feature: Dimelo::User#avatar_url

--- app/models/dimelo/user.rb
+++ app/models/dimelo/user.rb
@@ -1,13 +1,16 @@
 module Dimelo
   class User < Dimelo::API::Model
     
-    attr_accessor :avatar_url
     attributes :id, :firstname, :lastname, :signature, :email, :type, :username, :flow_state, :about, :avatar, :created_at, :updated_at
     submit_attributes :type, :firstname, :lastname, :email, :username, :avatar_url, :about
     
     has_many :memberships
     has_many :questions
     has_many :answers
+    
+    def avatar_url(size='normal')
+      avatar.try(:[], size).try(:[], 'url')
+    end
     
   end
 end
