5d5bff9fdba962837b42b1f8d0b2e7428f08b5bd
Renaud (Nel) Morvan <nel@w3fu.com>
2011-08-03 19:39:44 +0200

    feature: add updated_at for Dimelo::Feedback

--- lib/dimelo/api/model/feedback.rb
+++ lib/dimelo/api/model/feedback.rb
@@ -3,7 +3,7 @@ module Dimelo
     
     attributes :id, :title, :body, :body_format, :flow_state, :score, :user_id,
                :category_id, :category_ids, :status_id, :star, :comments_count, :positive_votes_count,
-               :negative_votes_count, :permalink, :created_at
+               :negative_votes_count, :permalink, :created_at, :updated_at
     
     submit_attributes :title, :body, :category_ids, :user_id
     
