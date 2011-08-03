c74dc4cef395579473dcc7846736ffbb37da2763
Renaud (Nel) Morvan <nel@w3fu.com>
2011-08-03 21:27:54 +0200

    feature: add updated_at for Dimelo::FeedbackComment

--- lib/dimelo/api/model/feedback_comment.rb
+++ lib/dimelo/api/model/feedback_comment.rb
@@ -3,7 +3,7 @@ module Dimelo
     
     path '/feedbacks/%{feedback_id}/comments/%{id}'
     
-    attributes :id, :feedback_id, :body, :body_format, :flow_state, :user_id, :type, :status_id, :created_at, :permalink
+    attributes :id, :feedback_id, :body, :body_format, :flow_state, :user_id, :type, :status_id, :created_at, :updated_at, :permalink
     submit_attributes :body, :user_id, :feedback_id
     
     belongs_to :user
