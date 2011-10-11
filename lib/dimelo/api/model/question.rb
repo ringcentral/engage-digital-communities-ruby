8e2d77b75f4009cb36b3baa1a9eb9231f4018580
Alexis Toulotte <al@alweb.org>
2011-10-11 15:18:17 +1100

    added category_names in question's attributes

--- lib/dimelo/api/model/question.rb
+++ lib/dimelo/api/model/question.rb
@@ -2,8 +2,9 @@ module Dimelo
   class Question < Dimelo::API::Model
     
     attributes :id, :title, :body, :body_format, :flow_state, :score, :user_id,
-               :category_id, :category_ids, :star, :permalink, :created_at, :updated_at,
-               :answers_count, :usefulnesses_yes_count, :usefulnesses_no_count
+               :category_id, :category_ids, :category_names, :star, :permalink,
+               :created_at, :updated_at, :answers_count,
+               :usefulnesses_yes_count, :usefulnesses_no_count
     
     submit_attributes :title, :body, :category_ids, :user_id
     
