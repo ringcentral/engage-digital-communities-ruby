035793d236e37a94745651faa0c687b28e5e90c3
Adrian Gheorghe <adrian.gheorghe@dimelo.com>
2012-02-03 11:12:09 +0100

    fix: Allow status_id to be set for Dimelo::FeedbackComment

--- lib/dimelo/api/model/feedback_comment.rb
+++ lib/dimelo/api/model/feedback_comment.rb
@@ -4,7 +4,7 @@ module Dimelo
     path '/feedbacks/%{feedback_id}/comments/%{id}'
     
     attributes :id, :feedback_id, :body, :body_format, :flow_state, :user_id, :type, :status_id, :created_at, :updated_at, :permalink
-    submit_attributes :body, :user_id, :feedback_id
+    submit_attributes :body, :user_id, :feedback_id, :status_id
     
     belongs_to :user
     belongs_to :feedback
