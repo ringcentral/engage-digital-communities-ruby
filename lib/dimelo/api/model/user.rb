bf6e67fde481bbdaab96393844476f3766981a4a
Vlad Zloteanu <vlad.zloteanu@dimelo.com>
2012-05-11 15:34:52 +0200

    added custom field attributes to user model - #29067511

--- lib/dimelo/api/model/user.rb
+++ lib/dimelo/api/model/user.rb
@@ -1,7 +1,11 @@
 module Dimelo
   class User < Dimelo::API::Model
-    
+    CUSTOM_FIELD_COUNT = 10
+    CUSTOM_FIELD_ATTRIBUTES = (1..CUSTOM_FIELD_COUNT).map { |i| "custom_field_#{i}".to_sym }.freeze
+
     attributes :id, :firstname, :lastname, :signature, :email, :type, :username, :flow_state, :about, :avatar, :created_at, :updated_at
+    attributes *CUSTOM_FIELD_ATTRIBUTES
+
     submit_attributes :type, :firstname, :lastname, :email, :username, :avatar_url, :about
     
     has_many :memberships
