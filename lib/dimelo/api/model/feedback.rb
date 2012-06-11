b137171c7542e0ad9d41207d90d2064b0f1408fa
Adrian Gheorghe <adrian.gheorghe@gmail.com>
2012-06-11 12:45:06 +0200

    feature: Added category_names field to feedbacks

--- lib/dimelo/api/model/feedback.rb
+++ lib/dimelo/api/model/feedback.rb
@@ -2,8 +2,9 @@ module Dimelo
   class Feedback < Dimelo::API::Model
     
     attributes :id, :title, :body, :body_format, :flow_state, :score, :user_id,
-               :category_id, :category_ids, :status_id, :star, :comments_count, :positive_votes_count,
-               :negative_votes_count, :permalink, :created_at, :updated_at, :ipaddr
+               :category_id, :category_ids, :category_names, :status_id, :star,
+               :comments_count, :positive_votes_count, :negative_votes_count,
+               :permalink, :created_at, :updated_at, :ipaddr
     
     submit_attributes :title, :body, :category_ids, :user_id
     
