95a5520a8f9e4bc5319ced47bb356ef0878be460
Vlad Zloteanu <vlad.zloteanu@dimelo.com>
2012-06-11 18:49:18 +0200

    fix: added question_flow_state on answers model

--- lib/dimelo/api/model/answer.rb
+++ lib/dimelo/api/model/answer.rb
@@ -3,7 +3,7 @@ module Dimelo
     
     path '/questions/%{question_id}/answers/%{id}'
     
-    attributes :id, :body, :body_format, :flow_state, :user_id, :stamp, :permalink, :created_at, :updated_at, :question_id, :comments_count, :ipaddr
+    attributes :id, :body, :body_format, :flow_state, :user_id, :stamp, :permalink, :created_at, :updated_at, :question_id, :comments_count, :ipaddr, :question_flow_state
     submit_attributes :body, :user_id, :question_id
     
     belongs_to :user
