46fb8d6c66fe48b55b02460672ea534940e6604e
Stephane Bounmy <stephanebounmy@gmail.com>
2012-07-25 18:35:30 +0200

    added author_stamp to method to answer

--- lib/dimelo/api/model/answer.rb
+++ lib/dimelo/api/model/answer.rb
@@ -1,21 +1,27 @@
 module Dimelo
   class Answer < Dimelo::API::Model
     include ::Dimelo::API::Common::Publishable
-    
+
     path '/questions/%{question_id}/answers/%{id}'
-    
+
     attributes :id, :body, :body_format, :flow_state, :user_id, :stamp, :permalink, :created_at, :updated_at, :question_id, :comments_count, :ipaddr, :question_flow_state
     submit_attributes :body, :user_id, :question_id
-    
+
     belongs_to :user
     belongs_to :question
-    
+
     def admin_stamp
       path = "#{compute_path(attributes)}/admin_stamp"
       response = client.transport(:post, path)
       self.attributes = Dimelo::API.decode_json(response)
       errors.empty?
     end
-    
+
+    def author_stamp
+      path = "#{compute_path(attributes)}/author_stamp"
+      response = client.transport(:post, path)
+      self.attributes = Dimelo::API.decode_json(response)
+      errors.empty?
+    end
   end
 end
