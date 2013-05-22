4477698a78c925db677ad770c3025b5dddef7a34
Adrian Gheorghe <adrian.gheorghe@gmail.com>
2013-05-22 15:38:54 +0300

    Add closed attribute for feedbacks and questions

--- lib/dimelo/api/model/feedback.rb
+++ lib/dimelo/api/model/feedback.rb
@@ -4,8 +4,8 @@ module Dimelo
 
     attributes :id, :title, :body, :body_format, :flow_state, :score, :user_id,
                :category_id, :category_ids, :category_names, :status_id, :star,
-               :comments_count, :positive_votes_count, :negative_votes_count,
-               :permalink, :created_at, :updated_at, :ipaddr
+               :closed, :comments_count, :positive_votes_count, :negative_votes_count,
+               :permalink, :ipaddr, :created_at, :updated_at
 
     submit_attributes :title, :body, :body_format, :category_ids, :user_id
 
--- lib/dimelo/api/model/question.rb
+++ lib/dimelo/api/model/question.rb
@@ -3,9 +3,9 @@ module Dimelo
     include ::Dimelo::API::Common::Publishable
 
     attributes :id, :title, :body, :body_format, :flow_state, :score, :user_id,
-               :category_id, :category_ids, :category_names, :star, :permalink,
-               :created_at, :updated_at, :answers_count,
-               :usefulnesses_yes_count, :usefulnesses_no_count, :ipaddr
+               :ipaddr, :category_id, :category_ids, :category_names, :answers_count,
+               :usefulnesses_yes_count, :usefulnesses_no_count, :star, :closed,
+               :permalink, :created_at, :updated_at
 
     submit_attributes :title, :body, :body_format, :category_ids, :user_id
 
