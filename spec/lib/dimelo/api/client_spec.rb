e3c10af577f52aee8a86a7098dcf1d0f4801f5c9
Stephane Bounmy <stephanebounmy@gmail.com>
2014-05-06 12:39:57 +0200

    improve: better api error #RD-4301

--- spec/lib/dimelo/api/client_spec.rb
+++ spec/lib/dimelo/api/client_spec.rb
@@ -3,9 +3,17 @@ require 'spec_helper'
 describe Dimelo::API::Client do
 
   subject do
-    Dimelo::API::Client.new('https://domain-test.api.users.dimelo.com/1.0', 'access_token' => ::ACCESS_TOKEN, :http_options => {:timeout => 80})
+    Dimelo::API::Client.new('https://domain-test.api.users.dimelo.info:4433/1.0', 'access_token' => ::ACCESS_TOKEN, :http_options => {:timeout => 80})
   end
 
+  def response_error(status, error, message='')
+    double  success?: false,
+            body: %Q{{
+                    "error": "#{error}",
+                    "message": "#{message}",
+                    "status": #{status}
+                  }}
+  end
   describe '#transport' do
 
     it 'return an the response body' do
@@ -13,10 +21,45 @@ describe Dimelo::API::Client do
       json.should == {"success" => true}
     end
 
-    it 'raise if response is not a 2XX' do
+    it 'raise InvalidAccessTokenError if token is invalid' do
       expect{
         subject.transport(:get, 'check', {'access_token' => 'invalid'})
-      }.to raise_error(Dimelo::API::Error)
+      }.to raise_error(Dimelo::API::InvalidAccessTokenError)
+    end
+
+    it 'raise NotFoundError if path is invalid' do
+      expect{
+        subject.transport(:get, 'invalid_path', {'access_token' => 'invalid'})
+      }.to raise_error(Dimelo::API::NotFoundError)
+    end
+
+    it 'raise NotEnabledError if api is not enabled' do
+      subject.stub_chain(:connection, :perform) { response_error 403, 'api_not_enabled' }
+      expect{
+        subject.transport(:get, 'check', {'access_token' => 'my-token'})
+      }.to raise_error(Dimelo::API::NotEnabledError)
+    end
+
+    it 'raise SSLError if request should be https' do
+      subject.stub_chain(:connection, :perform) { response_error 412, 'routing_error' }
+      expect{
+        subject.transport(:get, 'check', {'access_token' => 'my-token'})
+      }.to raise_error(Dimelo::API::SslError)
+    end
+
+    it 'raise InvalidUserTypeError if create user is invalid' do
+      subject.stub_chain(:connection, :perform) { response_error 400, 'invalid_user_type' }
+      expect{
+        subject.transport(:post, '/users', {'access_token' => 'my-token'})
+      }.to raise_error(Dimelo::API::InvalidUserTypeError)
+    end
+
+    it 'raise DomainNotFoundError if request on invalid domain' do
+      pending 'wait for ccp to handle domain not found as 404'
+      client = Dimelo::API::Client.new('https://no-domain.api.users.dimelo.info:4433/1.0', 'access_token' => ::ACCESS_TOKEN, :http_options => {:timeout => 80})
+      expect{
+        client.transport(:get, '/check', {'access_token' => 'my-token'})
+      }.to raise_error(Dimelo::API::DomainNotFoundError)
     end
 
   end
