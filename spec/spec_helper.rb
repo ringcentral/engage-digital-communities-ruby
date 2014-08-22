44ea7089cf6fdcbc1413193945dca6a92a4641ae
Jérémy Carlier <jeremy.carlier@dimelo.com>
2014-08-22 12:16:27 +0200

    fix: update access tokens

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ begin
 rescue LoadError
   require 'rspec'
 end
-ACCESS_TOKEN = "6265b6a77b826145b102985eecb0b9da"
+ACCESS_TOKEN = "55d8ac94bfb93a71f1fb7ee730079e8a"
 require File.expand_path(File.dirname(__FILE__) + '/../lib/dimelo_api')
 Dir[File.dirname(__FILE__) + "/examples/**/*.rb"].sort.each { |f| require f }
